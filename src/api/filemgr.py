a66e19186278a4474d2b951dd5cf3caeaefecc4b
Renato Teixeira <rena2damas@gmail.com>
2022-07-07 19:12:19 +0100

    refactor: handle ValidationError in upload/download actions #wip

--- src/api/filemgr.py
+++ src/api/filemgr.py
@@ -229,7 +229,7 @@ class FileManagerDownload(Resource):
             utils.abort_with(403)
         except FileNotFoundError:
             utils.abort_with(404)
-        except OSError:
+        except (OSError, ValidationError):
             utils.abort_with(400)
 
 
@@ -278,7 +278,7 @@ class FileManagerUpload(Resource):
             utils.abort_with(403)
         except FileNotFoundError:
             utils.abort_with(404)
-        except OSError:
+        except (OSError, ValidationError):
             utils.abort_with(400)
 
 
