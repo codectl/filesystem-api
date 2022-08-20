b9665255d9980143e5dda93599cc16726bdd14c0
renatodamas <rena2damas@gmail.com>
2022-08-20 11:45:37 +0100

    test: add subsfolder in paths #wip

--- tests/unit/test_filemgr.py
+++ tests/unit/test_filemgr.py
@@ -10,29 +10,29 @@ def svc():
 
 class TestFilesystemSvc:
     def test_list_files(self, svc, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/file2.txt")
-        fs.create_file("/tmp/.file3.txt")
-        assert len(svc.list_files(path="/tmp", show_hidden=False)) == 2
-        assert len(svc.list_files(path="/tmp", show_hidden=True)) == 3
-        assert len(svc.list_files(path="/tmp", substr="file2")) == 1
+        fs.create_file("/tmp/files/file1.txt")
+        fs.create_file("/tmp/files/file2.txt")
+        fs.create_file("/tmp/files/.file3.txt")
+        assert len(svc.list_files(path="/tmp/files", show_hidden=False)) == 2
+        assert len(svc.list_files(path="/tmp/files", show_hidden=True)) == 3
+        assert len(svc.list_files(path="/tmp/files", substr="file2")) == 1
 
     def test_list_files_on_missing_file_raises_exception(self, svc):
         with pytest.raises(FileNotFoundError) as ex:
-            svc.list_files(path="/tmp/missing.txt")
+            svc.list_files(path="/tmp/files/missing.txt")
         assert "No such file or directory" in str(ex.value)
 
     def test_list_files_on_restricted_path_raises_exception(self, svc, fs):
-        fs.create_file("/tmp/root", st_mode=0o000)
+        fs.create_file("/tmp/files/root", st_mode=0o000)
         with pytest.raises(PermissionError) as ex:
-            svc.list_files(path="/tmp/root")
+            svc.list_files(path="/tmp/files/root")
         assert "Permission denied" in str(ex.value)
 
     def test_stats(self, svc, fs):
-        fs.create_file("/tmp/file.txt")
-        stats = svc.stats(path="/tmp/file.txt")
+        fs.create_file("/tmp/files/file.txt")
+        stats = svc.stats(path="/tmp/files/file.txt")
         assert stats["name"] == "file.txt"
-        assert stats["path"] == "/tmp/file.txt"
+        assert stats["path"] == "/tmp/files/file.txt"
         assert stats["type"] == ".txt"
         assert stats["isFile"] is True
         assert stats["hasChild"] is False
