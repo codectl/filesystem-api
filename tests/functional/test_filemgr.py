79b2379e11b59a54da66b58397c3baf8150e257c
renatodamas <rena2damas@gmail.com>
2022-08-20 11:43:38 +0100

    test: fix tests #wip

--- tests/functional/test_filemgr.py
+++ tests/functional/test_filemgr.py
@@ -4,47 +4,48 @@ import json
 
 class TestFileManagerActions:
     def test_read_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/.file2.txt")
+        fs.create_file("/tmp/files/file1.txt")
+        fs.create_file("/tmp/files/.file2.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "read",
-                "path": "/tmp",
+                "path": "/tmp/files",
                 "showHiddenItems": True,
                 "data": [],
             },
         )
         data = response.json
         assert response.status_code == 200
-        assert data["cwd"]["name"] == "tmp"
-        assert data["cwd"]["path"] == "/tmp"
+        assert data["cwd"]["name"] == "files"
+        assert data["cwd"]["path"] == "/tmp/files"
         assert len(data["files"]) == 2
 
     def test_create_action(self, client, fs):
-        fs.create_dir("/tmp")
+        fs.create_dir("/tmp/dirs")
         response = client.post(
             "/file-manager/actions",
-            json={"action": "create", "path": "/tmp", "name": "dir", "data": []},
+            json={"action": "create", "path": "/tmp/dirs", "name": "dir", "data": []},
         )
         data = response.json
         assert response.status_code == 200
         assert len(data["files"]) == 1
         assert data["files"][0]["name"] == "dir"
-        assert data["files"][0]["path"] == "/tmp/dir"
+        assert data["files"][0]["path"] == "/tmp/dirs/dir"
         assert data["files"][0]["isFile"] is False
         assert data["files"][0]["hasChild"] is False
-        assert fs.exists("/tmp/dir")
+        assert fs.exists("/tmp/dirs/dir")
 
     def test_delete_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/file2.txt")
-        fs.create_file("/tmp/file3.txt")
+        base = "/tmp/files"
+        fs.create_file(f"{base}/file1.txt")
+        fs.create_file(f"{base}/file2.txt")
+        fs.create_file(f"{base}/file3.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "delete",
-                "path": "/tmp",
+                "path": base,
                 "names": ["file1.txt", "file2.txt"],
                 "data": [],
             },
@@ -52,19 +53,19 @@ class TestFileManagerActions:
         data = response.json
         assert response.status_code == 200
         assert len(data["files"]) == 2
-        assert any(file["path"] == "/tmp/file1.txt" for file in data["files"]) is True
-        assert any(file["path"] == "/tmp/file2.txt" for file in data["files"]) is True
-        assert fs.exists("/tmp/file1.txt") is False
-        assert fs.exists("/tmp/file2.txt") is False
-        assert fs.exists("/tmp/file3.txt") is True
+        assert any(file["path"] == f"{base}/file1.txt" for file in data["files"])
+        assert any(file["path"] == f"{base}/file2.txt" for file in data["files"])
+        assert fs.exists(f"{base}/file1.txt") is False
+        assert fs.exists(f"{base}/file2.txt") is False
+        assert fs.exists(f"{base}/file3.txt") is True
 
     def test_rename_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
+        fs.create_file("/tmp/files/file1.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "rename",
-                "path": "/tmp",
+                "path": "/tmp/files",
                 "name": "file1.txt",
                 "newName": "file2.txt",
                 "data": [],
@@ -74,19 +75,19 @@ class TestFileManagerActions:
         assert response.status_code == 200
         assert len(data["files"]) == 1
         assert data["files"][0]["name"] == "file2.txt"
-        assert data["files"][0]["path"] == "/tmp/file2.txt"
+        assert data["files"][0]["path"] == "/tmp/files/file2.txt"
         assert data["files"][0]["isFile"] is True
-        assert fs.exists("/tmp/file1.txt") is False
-        assert fs.exists("/tmp/file2.txt") is True
+        assert fs.exists("/tmp/files/file1.txt") is False
+        assert fs.exists("/tmp/files/file2.txt") is True
 
     def test_rename_existing_name_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/file2.txt")
+        fs.create_file("/tmp/files/file1.txt")
+        fs.create_file("/tmp/files/file2.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "rename",
-                "path": "/tmp",
+                "path": "/tmp/files",
                 "name": "file1.txt",
                 "newName": "file2.txt",
                 "data": [],
@@ -98,14 +99,15 @@ class TestFileManagerActions:
         assert "destination already exists" in data["error"]["message"]
 
     def test_search_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/.file2.txt")
-        fs.create_file("/tmp/test.txt")
+        base = "/tmp/files"
+        fs.create_file(f"{base}/file1.txt")
+        fs.create_file(f"{base}/.file2.txt")
+        fs.create_file(f"{base}/test.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "search",
-                "path": "/tmp",
+                "path": base,
                 "showHiddenItems": True,
                 "caseSensitive": True,
                 "searchString": "file",
@@ -115,16 +117,16 @@ class TestFileManagerActions:
         data = response.json
         assert response.status_code == 200
         assert len(data["files"]) == 2
-        assert any(file["path"] == "/tmp/file1.txt" for file in data["files"]) is True
-        assert any(file["path"] == "/tmp/.file2.txt" for file in data["files"]) is True
+        assert any(file["path"] == f"{base}/file1.txt" for file in data["files"])
+        assert any(file["path"] == f"{base}/.file2.txt" for file in data["files"])
 
     def test_file_details_action(self, client, fs):
-        fs.create_file("/tmp/file.txt")
+        fs.create_file("/tmp/files/file.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "details",
-                "path": "/tmp",
+                "path": "/tmp/files",
                 "names": ["file.txt"],
                 "data": [],
             },
@@ -132,33 +134,38 @@ class TestFileManagerActions:
         data = response.json
         assert response.status_code == 200
         assert data["details"]["name"] == "file.txt"
-        assert data["details"]["location"] == "/tmp/file.txt"
+        assert data["details"]["location"] == "/tmp/files/file.txt"
         assert data["details"]["isFile"] is True
         assert data["details"]["multipleFiles"] is False
         assert data["details"]["size"] == "0 B"
 
     def test_dir_details_action(self, client, fs):
-        fs.create_dir("/tmp/dir")
+        fs.create_dir("/tmp/dirs/dir")
         response = client.post(
             "/file-manager/actions",
-            json={"action": "details", "path": "/tmp", "names": ["dir"], "data": []},
+            json={
+                "action": "details",
+                "path": "/tmp/dirs",
+                "names": ["dir"],
+                "data": [],
+            },
         )
         data = response.json
         assert response.status_code == 200
         assert data["details"]["name"] == "dir"
-        assert data["details"]["location"] == "/tmp/dir"
+        assert data["details"]["location"] == "/tmp/dirs/dir"
         assert data["details"]["isFile"] is False
         assert data["details"]["multipleFiles"] is False
         assert data["details"]["size"] == "0 B"
 
     def test_multiple_files_details_action(self, client, fs):
-        fs.create_dir("/tmp/dir")
-        fs.create_dir("/tmp/file.txt")
+        fs.create_dir("/tmp/files/dir")
+        fs.create_file("/tmp/files/file.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "details",
-                "path": "/tmp",
+                "path": "/tmp/files",
                 "names": ["dir", "file.txt"],
                 "data": [],
             },
@@ -166,23 +173,23 @@ class TestFileManagerActions:
         data = response.json
         assert response.status_code == 200
         assert data["details"]["name"] == "dir, file.txt"
-        assert data["details"]["location"] == "All in /tmp"
+        assert data["details"]["location"] == "All in /tmp/files"
         assert data["details"]["isFile"] is False
         assert data["details"]["multipleFiles"] is True
         assert data["details"]["size"] == "0 B"
 
     def test_copy_action(self, client, fs):
-        fs.create_file("/tmp/src/file1.txt")
-        fs.create_file("/tmp/src/file2.txt")
-        fs.create_file("/tmp/dst/file2.txt")
+        fs.create_file("/tmp/files/src/file1.txt")
+        fs.create_file("/tmp/files/src/file2.txt")
+        fs.create_file("/tmp/files/dst/file2.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "copy",
-                "path": "/tmp/src",
+                "path": "/tmp/files/src",
                 "names": ["file1.txt", "file2.txt"],
                 "renameFiles": [],
-                "targetPath": "/tmp/dst",
+                "targetPath": "/tmp/files/dst",
                 "targetData": None,
                 "data": [],
             },
@@ -192,22 +199,22 @@ class TestFileManagerActions:
         assert len(data["files"]) == 2
         assert any(file["name"] == "file1.txt" for file in data["files"]) is True
         assert any(file["name"] == "file2 (1).txt" for file in data["files"]) is True
-        assert fs.exists("/tmp/src/file1.txt") is True
-        assert fs.exists("/tmp/dst/file2.txt") is True
-        assert fs.exists("/tmp/dst/file2 (1).txt") is True
+        assert fs.exists("/tmp/files/src/file1.txt") is True
+        assert fs.exists("/tmp/files/dst/file2.txt") is True
+        assert fs.exists("/tmp/files/dst/file2 (1).txt") is True
 
     def test_move_action(self, client, fs):
-        fs.create_file("/tmp/src/file1.txt")
-        fs.create_file("/tmp/src/file2.txt")
-        fs.create_file("/tmp/dst/file2.txt")
+        fs.create_file("/tmp/files/src/file1.txt")
+        fs.create_file("/tmp/files/src/file2.txt")
+        fs.create_file("/tmp/files/dst/file2.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "move",
-                "path": "/tmp/src",
+                "path": "/tmp/files/src",
                 "names": ["file1.txt", "file2.txt"],
                 "renameFiles": [],
-                "targetPath": "/tmp/dst",
+                "targetPath": "/tmp/files/dst",
                 "targetData": None,
                 "data": [],
             },
@@ -216,25 +223,25 @@ class TestFileManagerActions:
         assert response.status_code == 200
         assert len(data["files"]) == 1
         assert data["files"][0]["name"] == "file1.txt"
-        assert data["files"][0]["path"] == "/tmp/dst/file1.txt"
+        assert data["files"][0]["path"] == "/tmp/files/dst/file1.txt"
         assert data["error"]["code"] == 400
         assert data["error"]["message"] == "File Already Exists"
         assert data["error"]["fileExists"] == ["file2.txt"]
-        assert fs.exists("/tmp/src/file1.txt") is False
-        assert fs.exists("/tmp/src/file2.txt") is True
-        assert fs.exists("/tmp/dst/file1.txt") is True
+        assert fs.exists("/tmp/files/src/file1.txt") is False
+        assert fs.exists("/tmp/files/src/file2.txt") is True
+        assert fs.exists("/tmp/files/dst/file1.txt") is True
 
     def test_override_move_action(self, client, fs):
-        fs.create_file("/tmp/src/file.txt")
-        fs.create_file("/tmp/dst/file.txt")
+        fs.create_file("/tmp/files/src/file.txt")
+        fs.create_file("/tmp/files/dst/file.txt")
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "move",
-                "path": "/tmp/src",
+                "path": "/tmp/files/src",
                 "names": ["file.txt"],
                 "renameFiles": ["file.txt"],
-                "targetPath": "/tmp/dst",
+                "targetPath": "/tmp/files/dst",
                 "targetData": None,
                 "data": [],
             },
@@ -243,10 +250,10 @@ class TestFileManagerActions:
         assert response.status_code == 200
         assert len(data["files"]) == 1
         assert data["files"][0]["name"] == "file (1).txt"
-        assert data["files"][0]["path"] == "/tmp/dst/file (1).txt"
-        assert fs.exists("/tmp/src/file.txt") is False
-        assert fs.exists("/tmp/dst/file.txt") is True
-        assert fs.exists("/tmp/dst/file (1).txt") is True
+        assert data["files"][0]["path"] == "/tmp/files/dst/file (1).txt"
+        assert fs.exists("/tmp/files/src/file.txt") is False
+        assert fs.exists("/tmp/files/dst/file.txt") is True
+        assert fs.exists("/tmp/files/dst/file (1).txt") is True
 
     def test_missing_path_sends_error(self, client, fs):
         fs.create_dir("/tmp/files")
@@ -265,12 +272,12 @@ class TestFileManagerActions:
         assert data["error"]["message"] == "File Not Found"
 
     def test_permission_denied_sends_error(self, client, fs):
-        fs.create_file("/tmp/root.txt", st_mode=0o000)
+        fs.create_file("/tmp/files/root.txt", st_mode=0o000)
         response = client.post(
             "/file-manager/actions",
             json={
                 "action": "read",
-                "path": "/tmp/root.txt",
+                "path": "/tmp/files/root.txt",
                 "showHiddenItems": True,
                 "data": [],
             },
@@ -283,14 +290,14 @@ class TestFileManagerActions:
 
 class TestFileManagerDownload:
     def test_single_file_download_action(self, client, fs):
-        fs.create_file("/tmp/file.txt")
+        fs.create_file("/tmp/files/file.txt")
         response = client.post(
             "/file-manager/download",
             data={
                 "downloadInput": json.dumps(
                     {
                         "action": "download",
-                        "path": "/tmp",
+                        "path": "/tmp/files",
                         "names": ["file.txt"],
                         "data": [],
                     }
@@ -303,8 +310,8 @@ class TestFileManagerDownload:
         assert headers["Content-Type"] == "text/plain; charset=utf-8"
 
     def test_multiple_files_download_action(self, client, fs):
-        fs.create_file("/tmp/file1.txt")
-        fs.create_file("/tmp/file2.txt")
+        fs.create_file("/tmp/files/file1.txt")
+        fs.create_file("/tmp/files/file2.txt")
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         response = client.post(
             "/file-manager/download",
@@ -313,7 +320,7 @@ class TestFileManagerDownload:
                 "downloadInput": json.dumps(
                     {
                         "action": "download",
-                        "path": "/tmp",
+                        "path": "/tmp/files",
                         "names": ["file1.txt", "file2.txt"],
                         "data": [],
                     }
@@ -380,10 +387,10 @@ class TestFileManagerUpload:
 
 class TestFileManagerImages:
     def test_get_image(self, client, fs):
-        fs.create_file("/tmp/img.jpeg")
+        fs.create_file("/tmp/files/img.jpeg")
         response = client.get(
             "/file-manager/images",
-            query_string={"path": "/tmp/img.jpeg"},
+            query_string={"path": "/tmp/files/img.jpeg"},
         )
         headers = response.headers
         assert response.status_code == 200
