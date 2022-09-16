04a811a5e6e4be21d86d2181af2786cd94cc31fa
renatodamas <rena2damas@gmail.com>
2022-09-16 13:52:25 +0100

    chore: improvements on sample script #minor

--- samples/client.py
+++ samples/client.py
@@ -10,7 +10,7 @@ username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
 
 # create a sample temporary file
-directory = "/tmp"
+directory = "tmp"
 filename = "sample.txt"
 content = b"This is a sample"
 tmp = tempfile.TemporaryFile()
@@ -19,7 +19,7 @@ tmp.seek(0)
 
 # POST request
 r = requests.post(
-    url=f"{base_url}/filesystem/{directory}",
+    url=f"{base_url}/{directory}",
     headers={"accept": "application/json"},
     auth=(username, password),
     files={"files": (filename, tmp)},
@@ -31,7 +31,7 @@ assert r.status_code in (201, 400)  # file may already exist
 
 # GET request (json format)
 r = requests.get(
-    url=f"{base_url}/filesystem/{directory}/",
+    url=f"{base_url}/{directory}/",
     headers={"accept": "application/json"},
     auth=(username, password),
 )
@@ -42,7 +42,7 @@ assert "sample.txt" in r.json()
 # GET request (bytes format)
 # works for dirs too, but careful with dir size!
 r = requests.get(
-    url=f"{base_url}/filesystem/{directory}/{filename}",
+    url=f"{base_url}/{directory}/{filename}",
     headers={"accept": "application/octet-stream"},
     auth=(username, password),
     stream=True,
