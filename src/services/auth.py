951d6db82e5b81432574d03997da5380ae7a2f0e
renatodamas <rena2damas@gmail.com>
2022-09-20 18:58:10 +0100

    refactor: make decorator class scoped #wip

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,5 +1,6 @@
 import os
 import functools
+import re
 import sys
 import traceback
 
@@ -7,7 +8,7 @@ from multiprocessing import Process, Queue, get_context
 
 from src import utils
 
-__all__ = ("AuthSvc", "process")
+__all__ = ("AuthSvc", "as_user")
 
 
 class AuthSvc:
@@ -16,37 +17,21 @@ class AuthSvc:
         return True
 
 
-def target(queue, el):
-    queue.put(el)
+def target(queue, fn, *args, **kwargs):
+    queue.put(fn(*args, **kwargs))
 
 
-def process():
-    def wrapper(fn):
-        ctx = get_context("spawn")
-        queue = ctx.Queue(maxsize=1)
-
-        @functools.wraps(fn)
-        def decorated(*args, **kwargs):
-            ret = fn(*args, **kwargs)
-            ctx.Process(target=target, args=(queue, ret)).start()
-            return queue.get()
-
-        return decorated
-
-    return wrapper
-
-
-def impersonate(username=None):
+def as_user(arg=None, username=None):
     """Run a routing under user privileges."""
 
-    class UserCtx:
-        def __init__(self, usrname):
-            self.username = usrname
+    class run_as:
+        def __init__(self, user):
+            self.user = user
 
         def __enter__(self):
             try:
-                os.setuid(utils.user_uid(self.username))
-                os.setgid(utils.user_gid(self.username))
+                os.setuid(utils.user_uid(self.user))
+                os.setgid(utils.user_gid(self.user))
             except (KeyError, TypeError):
                 pass  # suppress missing username
             except PermissionError:
@@ -55,19 +40,29 @@ def impersonate(username=None):
         def __exit__(self, exc_type, exc_value, exc_traceback):
             pass
 
-    def wrapper(func):
-        @functools.wraps(func)
+    def fn_wrapper(fn):
+        ctx = get_context("spawn")
+        queue = ctx.Queue(maxsize=1)
+
+        @functools.wraps(fn)
         def decorated(*args, **kwargs):
             self = next(iter(args), None)
-            name = (
+            user = (
                 getattr(self, "username", None)
                 if not username and hasattr(self, "__class__")
                 else username
             )
 
-            with UserCtx(usrname=name):
-                return func(*args, **kwargs)
+            with run_as(user=user):
+                ctx.Process(target=target, args=(queue, fn, *args), kwargs=kwargs).start()
+                return queue.get()
 
         return decorated
 
-    return wrapper
+    def cls_wrapper(cls):
+        for attr in cls.__dict__:
+            if callable(getattr(cls, attr) and not re.match(r"__\w__", attr)):
+                setattr(cls, attr, fn_wrapper(getattr(cls, attr)))
+        return cls
+
+    return cls_wrapper(arg) if callable(arg) else cls_wrapper
--- src/services/filesystem.py
+++ src/services/filesystem.py
@@ -8,40 +8,35 @@ from pathlib import Path
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 
-from src.services.auth import process
+from src.services.auth import as_user
 
 __all__ = ("FilesystemSvc",)
 
 
+@as_user
 class FilesystemSvc:
     def __init__(self, username=None):
         self.username = str(username) if username else None
 
-    @process()
     def list(self, path, show_hidden=False) -> list[Path]:
         regex = r".*"
         if not show_hidden:
             regex = "".join((r"^(?!\.)", regex))
         return [p for p in Path(path).iterdir() if re.match(regex, p.name)]
 
-    @process()
     def stats(self, path) -> os.stat_result:
         return Path(path).stat()
 
-    @process()
     def save(self, dst, file: FileStorage):
         filename = secure_filename(file.filename)
         file.save(os.path.join(dst, filename))
 
-    @process()
     def mkdir(self, path):
         Path(path).mkdir()
 
-    @process()
     def exists(self, path):
         return Path(path).exists()
 
-    @process()
     def delete(self, path):
         p = Path(path)
         if p.is_dir():
@@ -49,18 +44,15 @@ class FilesystemSvc:
         else:
             p.unlink()
 
-    @process()
     def move(self, src, dst):
         src = Path(src)
         dst = self.rename_duplicates(dst=dst, filename=src.name)
         shutil.move(src, dst)
         return dst
 
-    @process()
     def rename(self, src, dst):
         Path(src).rename(dst)
 
-    @process()
     def copy(self, src, dst):
         src = Path(src)
         dst = self.rename_duplicates(dst=dst, filename=src.name)
@@ -70,7 +62,6 @@ class FilesystemSvc:
             shutil.copy2(src, dst)
         return dst
 
-    @process()
     def rename_duplicates(self, dst, filename, count=0):
         if count > 0:
             base, extension = os.path.splitext(filename)
@@ -83,7 +74,6 @@ class FilesystemSvc:
         else:
             return path
 
-    @process()
     def create_attachment(self, paths=()):
         obj = io.BytesIO()
         with tarfile.open(fileobj=obj, mode="w|gz") as tar:
@@ -93,6 +83,5 @@ class FilesystemSvc:
         obj.seek(0)
         return obj
 
-    @process()
     def is_file(self, path):
         return Path(path).is_file()
