18482a306d96127f7db69f914b8f10dd4f7eba59
Renato Damas <me@renatodamas.com>
2023-01-23 19:54:48 +0000

    style: code lint

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -39,10 +39,9 @@ def test_response():
         "code": 200,
         "description": "OK: test",
     }
-    assert http_response(code=400, description="error", serialize=False) == HTTPResponse(
-        code=400,
-        description="Bad Request: error"
-    )
+    assert http_response(
+        code=400, description="error", serialize=False
+    ) == HTTPResponse(code=400, description="Bad Request: error")
 
 
 def test_abort_with():
